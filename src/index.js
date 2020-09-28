09b6d0c3dfb67b8f41690ee7fe4be21b39f4d806
Cesar Hernandez <chernandez@tenna.com>
2020-09-28 15:28:42 -0500

    open for biz

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const BunnyBus = require('@tenna-llc/bunnybus');
 const Config = require('config');
 const Reader = require('./Reader');
 const Republish = require('./republish');
-const { logger } = require('./helpers');
+const { logger: Logger } = require('./helpers');
 
 module.exports = (async () => {
+    const logger = Logger();
     const bunnyBus = new BunnyBus(Config.bunnyBus);
-    const filename = await new Reader({ options: Config.queues, bunnyBus, logger: logger() }).start();
-    await new Republish({ options: Config.queues, bunnyBus, logger: logger() }).start({ filename });
+    const filename = await new Reader({ options: Config.queues, bunnyBus, logger }).start();
+    await new Republish({ options: Config.queues, bunnyBus, logger }).start({ filename });
 
-    process.exit(0);
+    await logger.flush();
+
+    console.log('DONE!!!!');
 })();
